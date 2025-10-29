2656bb373b1af50d812874917b41b2a01aca060e
mendy hershkop <hers.mm@gmail.com>
2025-10-29 10:23:02 +0200

    create validtion of guess

--- game/validate.py
+++ game/validate.py
@@ -1,1 +1,13 @@
-ECHO is on.
+def is_valid_guess(guess: str, length: int, *, unique_digits: bool = True) -> tuple[bool, str]:
+    if (len(guess))!=length:
+        return False, "Length is incorrect"
+    if not int(guess):
+        return False ,"Number only"
+    if len(set(guess)) == length:
+        return True, "God guess"
+
+
+def is_new_guess(guess: str, history: set[str]) -> bool:
+    return guess in history
+
+
