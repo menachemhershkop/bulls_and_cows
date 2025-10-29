b3f8d8f12114c39e465d474ff10f7df25e6ace50
mendy hershkop <hers.mm@gmail.com>
2025-10-29 09:49:10 +0200

    create generate_secret function

--- game/secret.py
+++ game/secret.py
@@ -1,1 +1,9 @@
-ECHO is on.
+import random
+def generate_secret(length: int = 4, *, unique_digits: bool = True, allow_leading_zero: bool = False, rng: random.Random | None =  None) -> str:
+    secret_number=""
+    for i in range(length):
+        if not allow_leading_zero:
+            if unique_digits:
+                number=random.randint(1,9)
+                secret_number+=str(number)
+    return secret_number
